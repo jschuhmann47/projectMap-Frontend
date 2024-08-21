4db77494129b81c7b195735654f4f5fb8acb7090
T-J-Antonio <tantonio@frba.utn.edu.ar>
2024-08-20 21:56:32 -0300

    added img in forgot-password and register forms

--- src/views/RegisterView/index.jsx
+++ src/views/RegisterView/index.jsx
@@ -3,10 +3,14 @@ import React from 'react';
 import { Container } from 'styles/form';
 
 import RegisterForm from './components/RegisterForm';
+import { Box } from '@mui/material';
 
 const RegisterView = ({ onSubmit }) => {
   return (
     <Container>
+      <Box sx={{ width: '40%' }}>
+        <img src='projectmap-logo-transparent.png' width='100%' />
+      </Box>
       <RegisterForm onSubmit={onSubmit} />
     </Container>
   );
