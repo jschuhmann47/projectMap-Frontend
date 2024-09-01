ea3ff9a077cc811f3aabbae9355d03002330aeff
T-J-Antonio <tantonio@frba.utn.edu.ar>
2024-09-01 13:49:14 -0300

    finished fixing bug

--- src/redux/selectors/app.selector.js
+++ src/redux/selectors/app.selector.js
@@ -2,8 +2,15 @@ import { createSelector } from 'reselect';
 
 const getFodaLoading = (state) => state.foda.loading;
 const getPestelLoading = (state) => state.pestel.loading;
+const getPorterLoading = (state) => state.porter.loading;
+const getAnsoffLoading = (state) => state.ansoff.loading;
+const getMcKinseyLoading = (state) => state.mckinsey.loading;
+const getOkrLoading = (state) => state.okr.loading;
+const getBSLoading = (state) => state.balanceScorecard.loading;
+const getQuestionnaireLoading = (state) => state.questionnaire.loading;
 
 export const toolsLoadingSelector = createSelector(
-  [getFodaLoading, getPestelLoading],
-  (fodaLoading, pestelLoading) => fodaLoading || pestelLoading
+  [getFodaLoading, getPestelLoading, getPorterLoading, getAnsoffLoading, getMcKinseyLoading, getOkrLoading, getBSLoading, getQuestionnaireLoading],
+  (fodaLoading, pestelLoading, porterLoading, ansoffLoading, mcKinseyLoading, okrLoading, bsLoading, questionnaireLoading) =>
+    fodaLoading || pestelLoading || porterLoading || mcKinseyLoading || okrLoading || bsLoading || questionnaireLoading
 );
