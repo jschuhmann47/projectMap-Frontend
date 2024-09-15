b614760cd6d1030ddb28e4dd5508729ba2a8cc48
T-J-Antonio <tantonio@frba.utn.edu.ar>
2024-09-15 13:47:18 -0300

    added multiline to inputv2

--- src/components/inputs/InputV2/index.jsx
+++ src/components/inputs/InputV2/index.jsx
@@ -9,6 +9,7 @@ export default function InputV2(props) {
     disable,
     fieldLabel,
     form,
+    multiline,
   } = props;
   const [showPassword, setShowPassword] = useState(false);
   const isPasswordInput = type === 'password';
@@ -29,6 +30,8 @@ export default function InputV2(props) {
         {...field}
         disabled={disable}
         type={getInputType()}
+        minRows={3}
+        maxRows={3}
         sx={{
           paddingLeft: '14px',
           paddingRight: '14px',
@@ -39,6 +42,7 @@ export default function InputV2(props) {
           width: '100%',
           borderColor: getIn(form.errors, field.name) ? '#FF0000' : '#344345',
         }}
+        multiline={multiline}
       />
     </Box>
   )
--- src/views/DashboardView/ProjectForm/index.jsx
+++ src/views/DashboardView/ProjectForm/index.jsx
@@ -1,10 +1,7 @@
-import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { Field, Form, Formik } from 'formik';
 
-import Textarea from 'components/inputs/Textarea';
 import Button from 'components/commons/Button';
 
-import { Typography } from '@mui/material';
-import { Box } from '@mui/system';
 import { validateField } from 'helpers/validateField';
 import InputV2 from 'components/inputs/InputV2';
 
@@ -14,35 +11,17 @@ const ProjectForm = ({ onSubmit }) => (
       <Form onSubmit={handleSubmit}>
         <Field
           name="titulo"
-          type="text"
           fieldLabel="Nombre del proyecto"
           component={InputV2}
           validate={validateField}
         />
-        <Box sx={{ width: '100%', height: '140px' }}>
-          <Field
-            name="descripcion"
-            placeholder="Descripción"
-            component={Textarea}
-            validate={validateField}
-          />
-          <ErrorMessage name="descripcion">
-            {(msg) => (
-              <Typography
-                sx={{
-                  textAlign: 'left',
-                  color: 'red',
-                  marginLeft: 2,
-                  marginTop: '2px',
-                  fontSize: '14px',
-                }}
-              >
-                {msg}
-              </Typography>
-            )}
-          </ErrorMessage>
-        </Box>
-
+        <Field
+          name="descripcion"
+          fieldLabel="Descripción"
+          multiline
+          component={InputV2}
+          validate={validateField}
+        />
         <Button type="submit" color="primary">
           Crear
         </Button>
