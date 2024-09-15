3b91b37d925907c49d77f9afe2ea4e8db78ab7cb
Juani Schuhmann <76596375+jschuhmann47@users.noreply.github.com>
2024-09-15 23:56:25 +0000

    Feature/more colors (#19)
    * throw some colors
    
    * more pastel colors
    
    * add color to steps
    
    * 'life if beautiful with colors' -> gandhi, probably
    
    * remove default
    
    * rename stuff

--- src/containers/DashboardContainer.jsx
+++ src/containers/DashboardContainer.jsx
@@ -1,20 +1,25 @@
-import Loading from 'components/commons/Loading';
-import Modal from 'components/commons/Modal';
-import LayoutContainer from 'containers/LayoutContainer';
-import { getRandomInt } from 'helpers/randomNumber';
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { onCreate, onDelete, onGetAll, onSearch } from 'redux/actions/projects.actions';
-import { getUser } from 'redux/actions/user.actions';
-import DashboardView from 'views/DashboardView';
-import ProjectForm from 'views/DashboardView/ProjectForm';
+import Loading from "components/commons/Loading";
+import Modal from "components/commons/Modal";
+import LayoutContainer from "containers/LayoutContainer";
+import { getRandomInt } from "helpers/randomNumber";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import {
+  onCreate,
+  onDelete,
+  onGetAll,
+  onSearch,
+} from "redux/actions/projects.actions";
+import { getUser } from "redux/actions/user.actions";
+import DashboardView from "views/DashboardView";
+import ProjectForm from "views/DashboardView/ProjectForm";
 
 const DashboardContainer = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [isAddNewOpen, setAddNew] = useState(false);
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
   const { items, loading } = useSelector((state) => state.projects);
   const user = useSelector((state) => state.user.data);
 
@@ -24,18 +29,10 @@ const DashboardContainer = () => {
   }, [dispatch]);
 
   const onSubmit = (formData) => {
-    const colors = [
-      '#c7dad9',
-      '#9fc1bf',
-      '#719f9d',
-      '#67908e',
-      '#88a8a6',
-      '#a9c0bf',
-      '#568482'
-    ];
-
-    const random = getRandomInt(colors.length);
-    const color = colors[random];
+    function randomHSLA() {
+      return `hsla(${~~(360 * Math.random())}, 70%,  72%, 0.8)`;
+    }
+    const color = randomHSLA();
 
     dispatch(onCreate({ ...formData, color }));
     setAddNew(false);
@@ -51,12 +48,12 @@ const DashboardContainer = () => {
 
   function search() {
     dispatch(onSearch(searchText));
-  };
+  }
 
   function clearSearch() {
-    setSearchText('');
+    setSearchText("");
     dispatch(onGetAll());
-  };
+  }
 
   return (
     <LayoutContainer>
