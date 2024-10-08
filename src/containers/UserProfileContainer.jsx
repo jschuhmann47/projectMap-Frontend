0bf14b7bd015402df5ae01ed9f65b0c46e6c2896
pabloH <elpabli09@gmail.com>
2024-10-07 23:51:01 -0300

    boton-volver-mi-perfil

--- src/containers/UserProfileContainer.jsx
+++ src/containers/UserProfileContainer.jsx
@@ -2,16 +2,19 @@ import Loading from 'components/commons/Loading';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
-
 import { getUser, onEdit, onGetProfile } from 'redux/actions/user.actions';
 
 import ProfileView from 'views/ProfileView';
+import { Box, ButtonBase } from '@mui/material';
+import { ArrowBack } from '@mui/icons-material';
 
 import LayoutContainer from './LayoutContainer';
 
 const UserProfileContainer = () => {
   const { userId } = useParams();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const user = useSelector((state) => state.user.data);
   const profile = useSelector((state) => state.user.profile);
   const loading = useSelector((state) => state.user.loading);
@@ -43,8 +46,32 @@ const UserProfileContainer = () => {
 
   const loadingPosta = userId ? loadingProfile : loading;
 
+  const handleBack = () => {
+    navigate(-1);
+  };
+
   return (
     <LayoutContainer>
+      <ButtonBase
+        onClick={handleBack}
+        sx={{
+          position: 'absolute',
+          top: '20px',
+          left: '20px',
+          display: 'flex',
+          alignItems: 'center',
+          padding: '8px 12px',
+          borderRadius: '5px',
+          backgroundColor: '#2c3e50',
+          color: '#fff',
+          fontSize: '16px',
+          zIndex: 1,
+          margin: '3%'
+        }}
+      >
+        <ArrowBack />
+        Volver
+      </ButtonBase>
       {loadingPosta ? (
         <Loading isModalMode />
       ) : (
