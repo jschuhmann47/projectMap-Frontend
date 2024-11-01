972a429d20a9ff48ca1a5d7203454889c2535401
T-J-Antonio <tantonio@frba.utn.edu.ar>
2024-11-01 19:59:14 -0300

    added stage 3

--- src/containers/PDCA/PdcaContainer.jsx
+++ src/containers/PDCA/PdcaContainer.jsx
@@ -2,12 +2,13 @@ import { Grid } from "@mui/material";
 import LayoutContainer from "containers/LayoutContainer";
 import { DemingStage } from "helpers/enums/pdca";
 import { StageByTool, Tools } from "helpers/enums/steps";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import { onGetOne, onPatch } from "redux/actions/pdca.actions";
 import Stage1View from "views/PdcaView/stage1";
 import Stage2View from "views/PdcaView/stage2";
+import Stage3View from "views/PdcaView/stage3";
 
 export default function PdcaContainer() {
   const { id, pdcaId } = useParams()
@@ -43,35 +44,50 @@ export default function PdcaContainer() {
     dispatch(onPatch(pdcaId, formData))
   }
 
-  switch(demingStage) {
-    case DemingStage.Planificar:
-      return (
-        <LayoutContainer>
-          <Grid item sx={{ height: '100%', width: '100%' }}>
-            <Stage1View
-              loading={loading}
-              pdcaData={data}
-              onClickBack={onClickBack}
-              onAddAction={onAddAction}
-              onRemoveAction={onRemoveAction}
-              inputValue={inputValue}
-              setInputValue={(e) => setInputValue(e.target.value)}
-            />
-          </Grid>
-        </LayoutContainer>
-      )
-    case DemingStage.Hacer:
-      return (
-        <LayoutContainer>
-          <Grid item sx={{ height: '100%', width: '100%' }}>
-            <Stage2View
-              loading={loading}
-              pdcaData={data}
-              onClickBack={onClickBack}
-              onEditAction={onEditAction}
-            />
-          </Grid>
-        </LayoutContainer>
-      )
-  }
+  const stageView = useMemo(() => {
+    switch (demingStage) {
+      case DemingStage.Planificar:
+        return (
+          <Stage1View
+            loading={loading}
+            pdcaData={data}
+            onClickBack={onClickBack}
+            onAddAction={onAddAction}
+            onRemoveAction={onRemoveAction}
+            inputValue={inputValue}
+            setInputValue={(e) => setInputValue(e.target.value)}
+          />
+        )
+      case DemingStage.Hacer:
+        return (
+          <Stage2View
+            loading={loading}
+            pdcaData={data}
+            onClickBack={onClickBack}
+            onEditAction={onEditAction}
+          />
+        )
+      case DemingStage.Verificar:
+        return (
+          <Stage3View
+            loading={loading}
+            pdcaData={data}
+            onClickBack={onClickBack}
+            onEditAction={onEditAction}
+          />
+        )
+      case DemingStage.Actuar:
+        return (
+          <span>etapa 4</span>
+        )
+    }
+  }, [demingStage])
+
+  return (
+    <LayoutContainer>
+      <Grid item sx={{ height: '100%', width: '100%' }}>
+        {stageView}
+      </Grid>
+    </LayoutContainer>
+  )
 }
---
+++ src/views/PdcaView/stage3.jsx
@@ -0,0 +1,104 @@
+import { ArrowBack } from "@mui/icons-material";
+import { Box, IconButton, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+
+function ActionItem({ action, onClickEdit }) {
+  // I know this breaks the container/presentational pattern...
+  // but I think this component is too simple to break down into two
+  const [progress, setProgress] = useState(action.progress)
+
+  function edit() {
+    onClickEdit({
+      _id: action._id,
+      name: action.name,
+      responsible: action.responsible,
+      deadline: action.deadline,
+      progress: progress,
+    })
+  }
+  return (
+    <Box
+      sx={{
+        border: '1px solid #405C5E',
+        width: '95%',
+        display: 'grid',
+        gridTemplateColumns: '4fr 1fr',
+        padding: '5px 20px',
+      }}
+    >
+      <Box>
+        <Typography sx={{ fontFamily: 'Fira Sans', fontSize: 20 }}>
+          {action.name}
+        </Typography>
+        <Box sx={{ display: 'grid', gridTemplateColumns: '3fr 2fr' }}>
+          <Typography sx={{ fontFamily: 'Fira Sans', fontSize: 14 }}>
+            Responsable: {action.responsible}
+          </Typography>
+          <Typography sx={{ fontFamily: 'Fira Sans', fontSize: 14 }}>
+            Fecha límite: {action.deadline.substring(0, 10)}
+          </Typography>
+        </Box>
+      </Box>
+      <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
+        <Typography sx={{ fontFamily: 'Fira Sans', fontSize: 16 }}>
+          Cumplimiento (%)
+        </Typography>
+        <TextField
+          type='number'
+          value={progress}
+          onChange={(e) => setProgress(e.target.value)}
+          inputProps={{
+            style: { fontFamily: 'Fira Sans', fontSize: 14 },
+          }}
+          onBlur={edit}
+          size='small'
+          sx={{ width: '40%' }}
+        />
+      </Box>
+    </Box>
+  )
+}
+
+export default function Stage3View({
+  loading,
+  pdcaData,
+  onClickBack,
+  onEditAction,
+}) {
+  return (
+    <Box sx={{
+      marginLeft: '5%',
+      marginRight: '5%',
+      marginTop: '5%',
+      display: 'flex',
+      flexDirection: 'column',
+      width: '90%',
+      alignItems: 'center',
+    }}>
+      <Box sx={{
+        display: 'flex',
+        position: 'relative',
+        justifyContent: 'center',
+        marginBottom: '10px',
+        alignItems: 'center',
+        width: '100%',
+      }}>
+        <IconButton size="small" onClick={onClickBack} sx={{ position: 'absolute', left: 0 }}>
+          <ArrowBack />
+        </IconButton>
+        <Typography sx={{ fontFamily: 'Fira Sans', fontSize: 30, fontWeight: 'bold' }}>
+          {pdcaData.name}
+        </Typography>
+      </Box>
+      <Typography sx={{ fontFamily: 'Fira Sans', fontSize: 26, fontWeight: 'bold', marginBottom: '10px' }}>
+        Etapa 3: hacer
+      </Typography>
+      <Typography sx={{ fontFamily: 'Fira Sans', fontSize: 20, marginBottom: '5px' }}>
+        ¿Cuál es el grado de cumplimiento de cada acción?
+      </Typography>
+      {pdcaData.actions.map((a) =>
+        <ActionItem action={a} onClickEdit={onEditAction} />
+      )}
+    </Box>
+  )
+}
