81c9253b4cece0cea1becada553335c7a07bb254
Diego Anazonian <diegoanazonian@MacBook-Pro-de-Diego.local>
2024-11-03 12:45:12 -0300

    chore: remove consultoria

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -12,7 +12,6 @@ import okrReducer from 'redux/reducers/okr.reducer';
 import questionnaireReducer from 'redux/reducers/questionnaire.reducer';
 import commentsReducer from 'redux/reducers/comments.reducer';
 import mejoraContinuaReducer from 'redux/reducers/mejora.containua.reducer';
-import consultoraReducer from './consultora.reducer';
 
 export default combineReducers({
   user: userReducer,
@@ -27,5 +26,4 @@ export default combineReducers({
   comments: commentsReducer,
   questionnaire: questionnaireReducer,
   mejoraContinua: mejoraContinuaReducer,
-  consultora: consultoraReducer,
 });
